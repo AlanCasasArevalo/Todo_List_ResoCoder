c099cbd57bb225d73c9ddfb16b7d66f0fcf09c20
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2021-02-05 09:54:28 +0100

    [SETUP][FIRST COMMIT] * Gitignore was added * All structure was added

---
+++ android/app/src/main/kotlin/com/example/todo_list_reso_coder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_list_reso_coder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material_APP_BAR'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello world'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
